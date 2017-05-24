a6e903824ec7b0247709222659f42cc27d2c0e1b
bobzz-zone <bobzz.zone@gmail.com>
2017-05-24 23:47:33 +0700

    on item row delete update

--- accisetax/hooks.py
+++ accisetax/hooks.py
@@ -10,7 +10,7 @@ app_icon = "octicon octicon-file-text"
 app_color = "grey"
 app_email = "bobzz.zone@gmail.com"
 app_license = "MIT"
-app_version = "0.0.4"
+app_version = "0.0.5"
 # Includes in <head>
 # ------------------
 
